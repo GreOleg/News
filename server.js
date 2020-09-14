c9e59cb9b2f2d2570750d50238948bd144b4226d
GreOleg <gresik.oleg@yandex.by>
2020-09-14 21:15:45 +0300

    create db

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 
+const connectDb = require('./config/db');
 const auth = require('./src/routes/auth');
 
 const app = express();
@@ -12,3 +13,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
 });
+
+connectDb();
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,19 +1,37 @@
 const express = require("express");
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require("express-validator");
 
-const router = express.Router();
+const User = require("../../models/User");
 
-router.post("/register", [check("name", "name is required").not().isEmpty(),
-check("email", "please include a valid email").isEmail(),
-check("password", "please enter a password with 6 or more characters").isLength({min: 6})], (req, res) => {
-    const errors = validationResult(req);
+router.post("/register", [
+    check("name", "name is required").not().isEmpty(),
+    check("email", "please include a valid email").isEmail(),
+    check("password", "please enter a password with 6 or more characters").isLength({ min: 6 })], async (req, res) => {
+        const errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
-        return res.status(400).json({
-            errors: errors.array()
-        })
-    };
-});
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array()
+            })
+        };
+        const { name, email, password } = req.body;
+
+        try {
+            let user = await User.findOne({ email });
+
+            if (user) {
+                return res.status(400).json({ errors: [{ msg: 'User alredy exists' }] });
+            }
+        } catch (err) {
+            console.log(err.message);
+
+            res.status(500).send('Send error');
+        }
+    });
 
 router.post('/login', () => {
     console.log('login');
